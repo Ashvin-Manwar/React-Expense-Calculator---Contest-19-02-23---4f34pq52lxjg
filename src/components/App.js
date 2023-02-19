e7b6082bf11ae93b2d58c2f234ea1137254100e2
Ashvin Manwar <manwar.ashvin@gmail.com>
2023-02-19 13:26:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,21 @@ import React from 'react'
 import '../styles/App.css';
 
 const App = () => {
+  const [expenses,setExpenses]=useState(0);
+  const handle= () =>{
+    const a=document.createElement("li");
+    const b=document.getelementById("expenses-input").value;
+    a.innerHTML=b;
+    document.getElementById("wxpense-list").appendChild(a);
+    const c=b.indexOf('-');
+    const d=b.slice(c+1).trim();
+    setExpense(parseInt(d)+expense);
+  }
 
   return (
     <div id="main">
       <input id="expense-input" />
-      <button id="expense-button">Add Expense</button>
+      <button id="expense-button" onClick={handle}>Add Expense</button>
       <div id="expense-list">
       </div>
       <div id="total-expense">
