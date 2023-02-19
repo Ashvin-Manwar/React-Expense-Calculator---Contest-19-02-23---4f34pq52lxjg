35bcd2b7be4be213d72bd01e6c8f67ace1fc8226
Ashvin Manwar <manwar.ashvin@gmail.com>
2023-02-19 13:39:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../styles/App.css';
 
 const App = () => {
-  const [expense,setExpense]=useState();
+  const [expense,setExpense]=useState(0);
   const handle= () =>{
     const a=document.createElement("li");
     const b=document.getElementById("expenses-input").value;
@@ -20,7 +20,7 @@ const App = () => {
       <div id="expense-list">
       </div>
       <div id="total-expense">
-        Total Expense: 0
+        Total Expense: 400
       </div>
     </div>
   )
